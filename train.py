61640f3cc23ad0ac6caf318d3660b43e7c623902
Shohei Miyashita <sound39music@gmail.com>
2017-11-20 17:51:09 +0900

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,6 @@ Train the policy with finite-difference policy gradient
 
 import chainer
 import chainer.functions as F
-import cupy as cp
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -179,15 +178,6 @@ if __name__ == '__main__':
 
     plotter = Plotter()
 
-    for _ in range(1000):
-        policy = Policy(xp.asarray([-0.565921, -0.2087926, -2.46556049, -0.91585176, 0.33338258, 0.41990558, -5.056393, 0.57013692, 1.08717598, -0.2268253, 0.28361697, -0.76006312, -1.92836859, -3.53624301, -2.75193499, 1.73638419]))
-
-        game = game_manager.new_game(True)
-        action = policy(xp.asarray([game.current_state()])).data[0]
-        game.shoot(action)
-        while not game.is_terminal:
-            game.update()
-    
     for itr in range(0, const.ITERATION):
         current_expected_income = trainer.train()
         plotter.plot(current_expected_income)
